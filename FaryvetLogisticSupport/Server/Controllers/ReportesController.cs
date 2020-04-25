00adfca176febcb2d56e544550b5cdf2e0b19afb
MauricioCM27 <mauriciocampos119@gmail.com>
2020-04-24 22:41:01 -0600

    Reportes

--- FaryvetLogisticSupport/Server/Controllers/ReportesController.cs
+++ FaryvetLogisticSupport/Server/Controllers/ReportesController.cs
@@ -24,12 +24,15 @@ namespace FaryvetLogisticSupport.Server.Controllers
         public async Task<ActionResult<object>> Get(DateTime fechaInicio, DateTime fechaFinal)
         {
             object obj = await context.FLS_Entregas
-                .Where(x => x.fechaSalida >= fechaInicio && x.fechaLlegada <= fechaFinal)
+                .Where(x => x.fechaSalida >= fechaInicio && 
+                x.fechaLlegada <= fechaFinal &&
+                x.estado == "Entrega Finalizada")
                 .GroupBy(x => x.chofer)
                 .Select(g => new { chofer = g.Key, 
                     peso = g.Sum(i => i.peso), 
                     costo = g.Sum(i => i.costo), 
-                    kilometrajeRecorrido = g.Sum(i => i.kilometrajeLlegada) - g.Sum(i => i.kilometrajeSalida)})
+                    kilometrajeRecorrido = g.Sum(i => i.kilometrajeLlegada) - g.Sum(i => i.kilometrajeSalida),
+                    cantidadEntregas = g.Count()})
                 .ToListAsync();
             return obj;
         }
---
+++ FaryvetLogisticSupport/Shared/Models/Reporte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaryvetLogisticSupport.Shared.Models
+{
+    public class Reporte
+    {
+        public string chofer { get; set; }
+        public float peso { get; set; }
+        public float costo { get; set; }
+        public float kilometrajeRecorrido { get; set; }
+        public int cantidadEntregas { get; set; }
+    }
+}
