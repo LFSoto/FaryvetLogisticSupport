8f531338a958b36e69c3eb0c62aab881ac243ca1
MauricioCM27 <mauriciocampos119@gmail.com>
2020-05-30 15:10:16 -0600

    Comentarios

--- FaryvetLogisticSupport/Shared/Models/Conductor.cs
+++ FaryvetLogisticSupport/Shared/Models/Conductor.cs
@@ -7,10 +7,17 @@ using System.Text;
 
 namespace FaryvetLogisticSupport.Shared.Models
 {
+    /// <summary>
+    /// Clase Conductor.
+    /// Modelo para el manejo de conductores en la aplicación.
+    /// </summary>
     [Table("FLS_Conductores")]
     public class Conductor
     {
-
+        /// <summary>
+        /// Constructor de la clase Conductor
+        /// Inicializa la fechaVencimientoB, fechaVencimientoA y fechaDeContratacion con la fecha actual.
+        /// </summary>
         public Conductor()
         {
             fechaVencimientoB = DateTime.Now;
@@ -18,34 +25,44 @@ namespace FaryvetLogisticSupport.Shared.Models
             fechaDeContratacion = DateTime.Now;
         }
 
+        /// <value>Get y Set del atributo cedula</value>
         [Key]
         [Required(ErrorMessage = "El campo cedula es requerido")]
         public string cedula { get; set; }
 
+        /// <value>Get y Set del atributo nombre</value>
         [Required(ErrorMessage = "El campo nombre es requerido")]
         public string nombre { get; set; }
 
+        /// <value>Get y Set del atributo apellido1</value>
         [Required(ErrorMessage = "El campo apellido paterno es requerido")]
         public string apellido1 { get; set; }
 
+        /// <value>Get y Set del atributo apellido2</value>
         [Required(ErrorMessage = "El campo apellido materno es requerido")]
         public string apellido2 { get; set; }
 
+        /// <value>Get y Set del atributo licenciaB</value>
         [Range(0, 4, ErrorMessage = "Licencia no valida, El valor se debe encontrar dentro del rango: 0-4")]
         public int licenciaB { get; set; }
 
+        /// <value>Get y Set del atributo fechaVencimientoB</value>
         [Column(TypeName = "date")]
         public DateTime fechaVencimientoB { get; set; }
 
+        /// <value>Get y Set del atributo licenciaA</value>
         [Range(0, 3, ErrorMessage = "Licencia no valida, El valor se debe encontrar dentro del rango: 0-3")]
         public int licenciaA { get; set; }
 
+        /// <value>Get y Set del atributo fechaVencimientoA</value>
         [Column(TypeName = "date")]
         public DateTime fechaVencimientoA { get; set; }
 
+        /// <value>Get y Set del atributo estado</value>
         [Required]
         public string estado { get; set; }
 
+        /// <value>Get y Set del atributo fechaDeContratacion</value>
         [Required]
         [Column(TypeName = "date")]
         [CustomDataAnnotationDate]
--- FaryvetLogisticSupport/Shared/Models/Reporte.cs
+++ FaryvetLogisticSupport/Shared/Models/Reporte.cs
@@ -4,12 +4,25 @@ using System.Text;
 
 namespace FaryvetLogisticSupport.Shared.Models
 {
+    /// <summary>
+    /// Clase Reporte.
+    /// Modelo para el manejo de Reporte en la aplicación.
+    /// </summary>
     public class Reporte
     {
+        /// <value>Get y Set del atributo chofer</value>
         public string chofer { get; set; }
+
+        /// <value>Get y Set del atributo peso</value>
         public float peso { get; set; }
+
+        /// <value>Get y Set del atributo costo</value>
         public float costo { get; set; }
+
+        /// <value>Get y Set del atributo kilometrajeRecorrido</value>
         public float kilometrajeRecorrido { get; set; }
+
+        /// <value>Get y Set del atributo cantidadEntregas</value>
         public int cantidadEntregas { get; set; }
     }
 }
