9d94f3e14b8666d9d315da4a8fc665c4c5c528c8
MauricioCM27 <mauriciocampos119@gmail.com>
2020-05-31 15:09:22 -0600

    Update DivisionGeografica.cs

--- FaryvetLogisticSupport/Shared/Models/DivisionGeografica.cs
+++ FaryvetLogisticSupport/Shared/Models/DivisionGeografica.cs
@@ -7,23 +7,20 @@ using System.Text;
 namespace FaryvetLogisticSupport.Shared.Models
 {
 
-    /* 
-    * @class DivisionGeografica.
-    * @brief Modelo para el manejo de DivisionGeografica en la aplicación.
-    */
-   
+    /// <summary>
+    /// Clase DivisionGeografica.
+    /// Modelo para el manejo de DivisionGeografica en la aplicación.
+    /// </summary>
+
     [Table("FLS_DisionesGeograficas")]
     public class DivisionGeografica
     {
-        /* 
-         * @brief Get y Set del atributo codigoPostal
-         */
+        /// <value>Get y Set del atributo codigoPostal</value>
         [Key]
         [Required]
         public int codigoPostal { get; set; }
-        /* 
-       * @brief Get y Set del atributo ubicacion
-       */
+
+        /// <value>Get y Set del atributo ubicacion</value>
         [Required]
         public string ubicacion { get; set; }
     }
